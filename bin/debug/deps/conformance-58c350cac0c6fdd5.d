16f3af4c4404e93843a45265fbf89c59d658e831
Nnamdi Michael Okpala <obinexus@outlook.com>
2026-07-14 15:27:21 +0100

    bin

--- bin/debug/deps/conformance-58c350cac0c6fdd5.d
+++
@@ -1,9 +0,0 @@
-C:\Users\OBINexus\Projects\obielf\target\debug\deps\conformance-58c350cac0c6fdd5.d: tests\conformance.rs Cargo.toml
-
-C:\Users\OBINexus\Projects\obielf\target\debug\deps\libconformance-58c350cac0c6fdd5.rmeta: tests\conformance.rs Cargo.toml
-
-tests\conformance.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
--- bin/release/deps/obielf.d
+++
@@ -1,5 +0,0 @@
-C:\Users\OBINexus\Projects\obielf\target\release\deps\obielf.d: src\bin\obielf.rs
-
-C:\Users\OBINexus\Projects\obielf\target\release\deps\obielf.exe: src\bin\obielf.rs
-
-src\bin\obielf.rs:
