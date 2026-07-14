5f8e9b0adb493e0276ef9b5ce263c21326267642
Nnamdi Michael Okpala <obinexus@outlook.com>
2026-07-14 15:24:31 +0100

    obielf

--- obielf/codex-check/debug/deps/conformance-58ca68189ad17847.d
+++
@@ -1,5 +0,0 @@
-C:\Users\OBINexus\Projects\obielf\target\codex-check\debug\deps\conformance-58ca68189ad17847.d: tests\conformance.rs
-
-C:\Users\OBINexus\Projects\obielf\target\codex-check\debug\deps\conformance-58ca68189ad17847.exe: tests\conformance.rs
-
-tests\conformance.rs:
--- obielf/codex-check/debug/deps/obielf.d
+++
@@ -1,5 +0,0 @@
-C:\Users\OBINexus\Projects\obielf\target\codex-check\debug\deps\obielf.d: src\bin\obielf.rs
-
-C:\Users\OBINexus\Projects\obielf\target\codex-check\debug\deps\obielf.exe: src\bin\obielf.rs
-
-src\bin\obielf.rs:
